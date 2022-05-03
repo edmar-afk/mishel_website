86466e0ad67f83a087781e70ab7e21c262b19a3e
project2479 <jaywrsnp6@gmail.com>
2022-05-03 15:07:47 +0800

    first website

---
+++ gallery/video-list.js
@@ -0,0 +1,39 @@
+let allVideos = [
+   {
+      name: "Tiktok Dance Part I",
+      src: "1",
+      id: "vid_1"
+   },
+   
+   {
+      name: "Tiktok Dance Part II",
+      src: "2",
+      id: "vid_2"
+   },
+
+   {
+      name: "Tiktok Dance Part III",
+      src: "3",
+      id: "vid_3"
+   },
+
+   {
+      name: "Tiktok Dance Part IV",
+      src: "4",
+      id: "vid_4"
+   },
+
+   {
+      name: "Tiktok Dance Part V",
+      src: "5",
+      id: "vid_5"
+   },
+
+   {
+      name: "Tiktok Dance Part VI",
+      src: "6",
+      id: "vid_6"
+   },
+
+  
+]
